dc7c3fbea6626ac055f74571a46d78d548b95bd8
dkilfoyle <dkilfoyle@xtra.co.nz>
2016-10-13 00:18:28 +1300

    update

--- Chapter 07 Neural Networks and SVM/MLwR_v2_07.r
+++ Chapter 07 Neural Networks and SVM/MLwR_v2_07.r
@@ -1,18 +1,12 @@
----
-title: "Decision Trees and Rules"
-author: "Dean Kilfoyle"
-date: "6 June 2016"
-output: html_notebook
----
+##### Chapter 7: Neural Networks and Support Vector Machines -------------------
 
-```{r setup, include=FALSE}
-knitr::opts_chunk$set(echo = TRUE)
-```
+##### Part 1: Neural Networks -------------------
+## Example: Modeling the Strength of Concrete  ----
 
-## Neural Networks
-
-```{r loaddata}
-concrete <- read.csv("chapter07/concrete.csv")
+## Step 2: Exploring and preparing the data ----
+# read in data and examine structure
+concrete <- read.csv("concrete.csv")
+str(concrete)
 
 # custom normalization function
 normalize <- function(x) { 
@@ -22,35 +16,53 @@ normalize <- function(x) {
 # apply normalization to entire data frame
 concrete_norm <- as.data.frame(lapply(concrete, normalize))
 
+# confirm that the range is now between zero and one
+summary(concrete_norm$strength)
+
+# compared to the original minimum and maximum
+summary(concrete$strength)
+
 # create training and test data
 concrete_train <- concrete_norm[1:773, ]
 concrete_test <- concrete_norm[774:1030, ]
-```
 
-### Train the neuralnet model
- 
-```{r}
+## Step 3: Training a model on the data ----
+# train the neuralnet model
 library(neuralnet)
 
 # simple ANN with only a single hidden neuron
 set.seed(12345) # to guarantee repeatable results
-concrete_model <- neuralnet(formula = strength ~ cement + slag + ash + water + superplastic + coarseagg + fineagg + age, data = concrete_train)
+concrete_model <- neuralnet(formula = strength ~ cement + slag +
+                              ash + water + superplastic + 
+                              coarseagg + fineagg + age,
+                              data = concrete_train)
 
 # visualize the network topology
 plot(concrete_model)
-```
 
-### Evaluating model performance
-
-```{r}
+## Step 4: Evaluating model performance ----
 # obtain model results
 model_results <- compute(concrete_model, concrete_test[1:8])
 # obtain predicted strength values
 predicted_strength <- model_results$net.result
 # examine the correlation between predicted and actual values
 cor(predicted_strength, concrete_test$strength)
-```
 
+## Step 5: Improving model performance ----
+# a more complex neural network topology with 5 hidden neurons
+set.seed(12345) # to guarantee repeatable results
+concrete_model2 <- neuralnet(strength ~ cement + slag +
+                               ash + water + superplastic + 
+                               coarseagg + fineagg + age,
+                               data = concrete_train, hidden = 5)
+
+# plot the network
+plot(concrete_model2)
+
+# evaluate the results as we did before
+model_results2 <- compute(concrete_model2, concrete_test[1:8])
+predicted_strength2 <- model_results2$net.result
+cor(predicted_strength2, concrete_test$strength)
 
 ##### Part 2: Support Vector Machines -------------------
 ## Example: Optical Character Recognition ----
